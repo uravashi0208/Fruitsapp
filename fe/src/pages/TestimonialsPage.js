fc5d3df09101353dfce862ebe36c8fb2e0d80500
uravashi0208 <zeel129patel@gmail.com>
2025-09-10 20:24:32 +0200

    remove old folder

--- fe/src/pages/TestimonialsPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TestimonialsPage = () => {
-  return <div>{/* Testimonials component will be added here when available */}</div>;
-};
-
-export default TestimonialsPage;
